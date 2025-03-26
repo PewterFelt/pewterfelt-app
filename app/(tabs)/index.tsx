e7bb879d418f4b5adbf449863ebdaa2fb06cb4c3
João Inez <joaoinez.info@gmail.com>
2025-03-26 02:19:19 +0100

    feat: add basic text input with gesture handling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,29 +1,44 @@
 import { URLInput } from "@/components/URLInput";
-import { XCircle } from "phosphor-react-native";
+import { XCircle, CheckCircle } from "phosphor-react-native";
 import { useState } from "react";
-import { Dimensions, Text, TouchableOpacity, View } from "react-native";
+import {
+  Dimensions,
+  KeyboardAvoidingView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import {
   Gesture,
   GestureDetector,
   GestureHandlerRootView,
 } from "react-native-gesture-handler";
 import Animated, {
+  interpolate,
   runOnJS,
   useAnimatedStyle,
   useSharedValue,
   withSpring,
 } from "react-native-reanimated";
 
-const { height: SCREEN_HEIGHT } = Dimensions.get("window");
-const MAX_HEIGHT = SCREEN_HEIGHT - 180;
-const MIN_HEIGHT = 180;
-
 export default function HomeScreen() {
+  const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+  const MAX_HEIGHT = SCREEN_HEIGHT - 130;
+  const MIN_HEIGHT = 180;
+  const SPRING_CONFIG = {
+    damping: 20,
+    stiffness: 200,
+  };
+
   const [isExpanded, setIsExpanded] = useState(false);
   const height = useSharedValue(MIN_HEIGHT);
 
   const containerStyle = useAnimatedStyle(() => ({
     height: height.value,
+  }));
+
+  const headerStyle = useAnimatedStyle(() => ({
+    opacity: interpolate(height.value, [MIN_HEIGHT, MAX_HEIGHT], [0, 1]),
   }));
 
   const panGesture = Gesture.Pan()
@@ -36,14 +51,11 @@ export default function HomeScreen() {
     })
     .onEnd((event) => {
       if (event.translationY > 100 || event.velocityY > 500) {
-        height.value = withSpring(MIN_HEIGHT, { damping: 20, stiffness: 200 });
+        height.value = withSpring(MIN_HEIGHT, SPRING_CONFIG);
 
         runOnJS(setIsExpanded)(false);
       } else {
-        height.value = withSpring(MAX_HEIGHT, {
-          damping: 20,
-          stiffness: 200,
-        });
+        height.value = withSpring(MAX_HEIGHT, SPRING_CONFIG);
 
         runOnJS(setIsExpanded)(true);
       }
@@ -51,7 +63,7 @@ export default function HomeScreen() {
 
   const tapGesture = Gesture.Tap().onStart(() => {
     if (!isExpanded) {
-      height.value = withSpring(MAX_HEIGHT, { damping: 20, stiffness: 200 });
+      height.value = withSpring(MAX_HEIGHT, SPRING_CONFIG);
 
       runOnJS(setIsExpanded)(true);
     }
@@ -59,25 +71,29 @@ export default function HomeScreen() {
 
   return (
     <GestureHandlerRootView className="flex-1 bg-neutral-900">
-      {isExpanded && (
+      <Animated.View
+        className="absolute top-20 left-0 z-10 w-full flex-row items-center justify-center px-2"
+        style={headerStyle}
+      >
+        <Text className="dark:text-neutral-500 text-xl absolute">
+          Add a URL link
+        </Text>
         <TouchableOpacity
-          className="absolute top-16 right-4 z-10 bg-neutral-800 p-2 rounded-full"
+          className="dark:bg-neutral-800 p-2 rounded-full ml-auto"
           onPress={() => {
-            height.value = withSpring(MIN_HEIGHT, {
-              damping: 20,
-              stiffness: 200,
-            });
-            setIsExpanded(false);
+            height.value = withSpring(MIN_HEIGHT, SPRING_CONFIG);
+
+            runOnJS(setIsExpanded)(false);
           }}
         >
-          <XCircle size={24} color="white" />
+          <XCircle size={24} color="#f4a261" weight="duotone" />
         </TouchableOpacity>
-      )}
+      </Animated.View>
 
       <GestureDetector gesture={Gesture.Simultaneous(tapGesture, panGesture)}>
         <Animated.View
           className="absolute z-10 bottom-0 left-0 right-0 mx-2 rounded-t-2xl p-4 pt-2 shadow-top dark:bg-neutral-800"
-          style={[containerStyle]}
+          style={containerStyle}
         >
           <View className="w-10 h-1 rounded-full self-center mb-3 mt-1 bg-pewter-gray" />
 
@@ -87,11 +103,25 @@ export default function HomeScreen() {
 
           {!isExpanded && (
             <Text className="absolute left-4 top-4 text-gray-400 text-base">
-              Tap to expand
+              Add a URL link
             </Text>
           )}
         </Animated.View>
       </GestureDetector>
+
+      {isExpanded && (
+        <KeyboardAvoidingView
+          className="absolute bottom-4 right-4 z-10"
+          behavior="padding"
+        >
+          <TouchableOpacity
+            className="bg-pewter-orange rounded-full p-2 mb-4"
+            onPress={() => null}
+          >
+            <CheckCircle size={24} color="white" weight="duotone" />
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+      )}
     </GestureHandlerRootView>
   );
 }
